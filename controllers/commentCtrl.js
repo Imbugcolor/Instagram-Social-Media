6412777d2de9db1949d702ccdd57847318e3b0a2
Imbugcolor <viethd123456@gmail.com>
2023-04-01 21:43:33 +0700

    Get Comments, Edit, Delete Comment, Like, Reply comment

--- controllers/commentCtrl.js
+++ controllers/commentCtrl.js
@@ -4,10 +4,10 @@ const Posts = require('../models/postModel')
 const commentCtrl = {
     createComment: async(req, res) => {
         try {
-            const { postId, content, tag, reply } = req.body
+            const { postId, content, tag, reply, postUserId } = req.body
 
             const newComment = new Comments({
-                user: req.user._id, content, tag, reply
+                user: req.user._id, content, tag, reply, postUserId, postId
             })
 
             await Posts.findOneAndUpdate({_id: postId}, {
@@ -18,6 +18,65 @@ const commentCtrl = {
 
             res.json({newComment})
 
+        } catch (err) {
+            return res.status(500).json({msg: err.message})
+        }
+    },
+    updateComment: async(req, res) => {
+        try {
+           const { content } = req.body
+           
+           await Comments.findOneAndUpdate({_id: req.params.id, user: req.user._id}, {content})
+
+           res.json({msg: 'Update success!'})
+
+        } catch (err) {
+            return res.status(500).json({msg: err.message})
+        }
+    },
+    likeComment: async (req, res) => {
+        try {
+            const comment = await Comments.find({_id: req.params.id, likes: req.user._id})
+            if(comment.length > 0) return res.status(400).json({msg: 'You liked this comment.'})
+
+            await Comments.findOneAndUpdate({_id: req.params.id}, {
+                $push: {likes: req.user._id}
+            }, {new: true})
+
+            res.json({msg: 'Liked Comment!'})
+            
+        } catch (err) {
+            return res.status(500).json({msg: err.message})
+        }
+    },
+    unLikeComment: async (req, res) => {
+        try {
+        
+            await Comments.findOneAndUpdate({_id: req.params.id}, {
+                $pull: {likes: req.user._id}
+            }, {new: true})
+
+            res.json({msg: 'UnLiked Comment!'})
+            
+        } catch (err) {
+            return res.status(500).json({msg: err.message})
+        }
+    },
+    deleteComment: async (req, res) => {
+        try {
+            const comment = await Comments.findOneAndDelete({
+                _id: req.params.id,
+                $or: [
+                    {user: req.user._id},
+                    {postUserId: req.user._id}
+                ]
+            })
+
+            await Posts.findOneAndUpdate({_id: comment.postId},{
+                $pull: {comments: req.params.id}
+            })  
+
+            res.json({msg: 'Deleted Comment!'})
         } catch (err) {
             return res.status(500).json({msg: err.message})
         }
