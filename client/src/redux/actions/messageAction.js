f5e246ab97f43a55062046340a9ae681806a0b9d
Imbugcolor <viethd123456@gmail.com>
2023-04-11 11:14:22 +0700

    Add Message

--- client/src/redux/actions/messageAction.js
+++ client/src/redux/actions/messageAction.js
@@ -1,11 +1,16 @@
 
 
 export const MESS_TYPES = {
-    ADD_USER: 'ADD_USER'
+    ADD_USER: 'ADD_USER',
+    ADD_MESSAGE: 'ADD_MESSAGE'
 }
 
 export const addUser = ({user, message}) => async(dispatch) => {
     if(message.users.every(item => item._id !== user._id)){
         dispatch({type: MESS_TYPES.ADD_USER, payload: user})
     }
+}
+
+export const addMessage = ({msg, auth, socket}) => async(dispatch) => {
+    dispatch({type: MESS_TYPES.ADD_MESSAGE, payload: msg})
 }
